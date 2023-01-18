96a3e5adb9a58e7e2286c0f98bb8c7bb69d4ce1b
baburao7975 <115793394+baburao7975@users.noreply.github.com>
2023-01-18 19:58:03 +0530

    Update create-instance.tf

--- create-instance.tf
+++ create-instance.tf
@@ -1,15 +1,40 @@
 provider "aws" {
-  region      =   "us-east-1"
- access_key  =   "${access_key}"
- secret_key  =   "${secret_key}"
+region = "us-east-1"
+access_key = "${access_key}"
+secret_key = "${secret_key}"
 }
 
-resource "aws_instance" "myinstance" {
-  ami = "ami-0b93ce03dcbcb10f6"
-  instance_type = "t2.micro"
-  key_name= "newbabu"
-  
-  tags = {
-    Name = "Terra_Docker"
+resource "aws_instance" "ec2" {
+    ami = "ami-06878d265978313ca"
+    instance_type = "t2.micro"
+    key_name= "newbabu"
+}
+
+resource "aws_security_group" "main" {
+  egress = [
+    {
+      cidr_blocks      = [ "0.0.0.0/0", ]
+      description      = ""
+      from_port        = 0
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "-1"
+      security_groups  = []
+      self             = false
+      to_port          = 0
+    }
+  ]
+ ingress                = [
+   {
+     cidr_blocks      = [ "0.0.0.0/0", ]
+     description      = ""
+     from_port        = 22
+     ipv6_cidr_blocks = []
+     prefix_list_ids  = []
+     protocol         = "tcp"
+     security_groups  = []
+     self             = false
+     to_port          = 22
   }
+  ]
 }
