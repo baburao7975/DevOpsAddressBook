89becbfee7063686b3421a85291841b2012aafba
baburao7975 <115793394+baburao7975@users.noreply.github.com>
2023-01-18 19:18:28 +0530

    Update create-instance.tf

--- create-instance.tf
+++ create-instance.tf
@@ -1,3 +1,9 @@
+provider "aws" {
+  region      =   "us-east-1"
+ access_key  =   "AKIA4OVKFWLGEWN2G35T"
+ secret_key  =   "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"
+}
+
 resource "aws_instance" "ec2" {
   ami           = "ami-0b93ce03dcbcb10f6"
   instance_type = "t2.micro"
