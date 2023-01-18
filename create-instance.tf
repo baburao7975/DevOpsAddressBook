e513726cfd7ef88a5e5da5a3cc41b30529d96ca2
baburao7975 <115793394+baburao7975@users.noreply.github.com>
2023-01-18 19:43:57 +0530

    Update create-instance.tf

--- create-instance.tf
+++ create-instance.tf
@@ -1,13 +1,13 @@
 provider "aws" {
   region      =   "us-east-1"
- access_key  =   "${access_key}"
- secret_key  =   "${secret_key}"
+ access_key  =   "AKIA4OVKFWLGEWN2G35T"
+ secret_key  =   "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"
 }
 
 resource "aws_instance" "myinstance" {
-  ami           = "ami-0b93ce03dcbcb10f6"
+  ami = "ami-0b93ce03dcbcb10f6"
   instance_type = "t2.micro"
-  key_name      = "newbabu"
+  key_name= "newbabu"
   
   tags = {
     Name = "Terra_Docker"
