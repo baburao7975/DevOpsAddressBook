86b517192dafee62ff5ea7fca7b9315e6bf8308a
baburao7975 <115793394+baburao7975@users.noreply.github.com>
2023-01-18 19:13:09 +0530

    Update create-instance.tf

--- create-instance.tf
+++ create-instance.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "MyInstance" {
-  ami           = "ami-06878d265978313ca"
-  instance_type = "t2.small"
+  ami           = "ami-0b93ce03dcbcb10f6"
+  instance_type = "t2.micro"
   key_name      = "newbabu"
   
   tags = {
