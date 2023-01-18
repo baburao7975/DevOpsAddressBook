2e1df591e222b2db33794937a437378bb8d0e475
baburao7975 <115793394+baburao7975@users.noreply.github.com>
2023-01-18 20:08:40 +0530

    Update create-instance.tf

--- create-instance.tf
+++ create-instance.tf
@@ -1,40 +1,6 @@
-provider "aws" {
-region = "us-east-1"
-access_key = "AKIA4OVKFWLGEWN2G35T"
-secret_key = "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"
-}
-
 resource "aws_instance" "ec2" {
     ami = "ami-06878d265978313ca"
     instance_type = "t2.micro"
     key_name= "newbabu"
 }
 
-resource "aws_security_group" "main" {
-  egress = [
-    {
-      cidr_blocks      = [ "0.0.0.0/0", ]
-      description      = ""
-      from_port        = 0
-      ipv6_cidr_blocks = []
-      prefix_list_ids  = []
-      protocol         = "-1"
-      security_groups  = []
-      self             = false
-      to_port          = 0
-    }
-  ]
- ingress                = [
-   {
-     cidr_blocks      = [ "0.0.0.0/0", ]
-     description      = ""
-     from_port        = 22
-     ipv6_cidr_blocks = []
-     prefix_list_ids  = []
-     protocol         = "tcp"
-     security_groups  = []
-     self             = false
-     to_port          = 22
-  }
-  ]
-}
