6d9aa7c7708efd7a323913020002bdd5d5bf1b0e
baburao7975 <115793394+baburao7975@users.noreply.github.com>
2023-01-18 15:08:31 +0530

    Update create-instance.tf

--- create-instance.tf
+++ create-instance.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "MyInstance" {
-  ami           = "ami-0b93ce03dcbcb10f6"
+  ami           = "ami-06878d265978313ca"
   instance_type = "t2.micro"
-  key_name      = "babukey"
+  key_name      = "newbabu"
   
   tags = {
     Name = "Terraform_Docker"
