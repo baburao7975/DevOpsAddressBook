add96ed0fae8c5897f6e09158ff99451e32dd6b0
baburao7975 <115793394+baburao7975@users.noreply.github.com>
2023-01-18 22:15:19 +0530

    Create docker.sh

---
+++ docker.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+
+sudo yum update -y
+sudo amazon-linux-extras install docker
+sudo systemctl start docker
+sudo usermod -a -G docker ec2-user
+docker info
+sudo systemctl enable docker
