9d0b027fa273a010e763f5392ad6df843f301a94
baburao7975 <115793394+baburao7975@users.noreply.github.com>
2023-01-18 19:49:50 +0530

    Update create-instance.tf

--- create-instance.tf
+++ create-instance.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region      =   "us-east-1"
- access_key  =   "AKIA4OVKFWLGEWN2G35T"
- secret_key  =   "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"
+ access_key  =   "${access_key}"
+ secret_key  =   "${secret_key}"
 }
 
 resource "aws_instance" "myinstance" {
