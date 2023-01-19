15de8a62bae646a9f3af680e25a1e0163e0d6638
baburao7975 <115793394+baburao7975@users.noreply.github.com>
2023-01-19 21:44:39 +0530

    Update login.java

--- src/test/java/com/edurekademo/utilities/login.java
+++ src/test/java/com/edurekademo/utilities/login.java
@@ -1,11 +1,47 @@
-package com.selenium;
+package com.company;
 
+import org.junit.Assert;
+import java.io.File;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.testng.annotations.BeforeTest;  
+import org.testng.annotations.Test; 
+import org.testng.annotations.AfterTest;
+import org.openqa.selenium.By;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class App {
+
+
+public class Login {
+
+    WebDriver driver;
+    @BeforeTest
+    public void setup(){
+		System.setProperty("WebDriver.chrome.driver","/root/chromedriver");
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--no-sandbox"); // Bypass OS security model, MUST BE THE VERY FIRST OPTION
+        options.addArguments("--headless");
+        options.setExperimentalOption("useAutomationExtension", false);
+        options.addArguments("start-maximized"); // open Browser in maximized mode
+        options.addArguments("disable-infobars"); // disabling infobars
+        options.addArguments("--disable-extensions"); // disabling extensions
+        options.addArguments("--disable-gpu"); // applicable to windows os only
+        options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
+        driver = new ChromeDriver(options); //initialise the WebDriver
+    }
+}
+	    
+	    public class App {
 	
 	public static void main(String[] args) {
-		System.setProperty("webdriver.chrome.driver", "/root/mypro/chromedriver");
+		System.setProperty("webdriver.chrome.driver", "/root/chromedriver");
 		WebDriver driver = new ChromeDriver();
 		driver.get("http://100.25.190.88:49153/addressbook/");
+	}
+	    }
+	    
+	    
