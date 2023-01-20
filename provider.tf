8a2fa0ba37cfdba9b58d97a1e536bfcb9841b886
baburao7975 <115793394+baburao7975@users.noreply.github.com>
2023-01-20 19:59:44 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
 region = "us-east-1"
-access_key = "AKIA4OVKFWLGLLT7TVE4"
-secret_key = "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"
+access_key = "AKIA4OVKFWLGFHNCI6MP"
+secret_key = "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"
 }
